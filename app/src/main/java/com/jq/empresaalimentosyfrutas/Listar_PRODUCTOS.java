27f0abeb35ea3bc0354c99587245f537bd7c4946
Juan <juan.3801911647@ucaldas.edu.co>
2021-09-13 22:35:46 -0500

    Version 1.0

---
+++ app/src/main/java/com/jq/empresaalimentosyfrutas/Listar_PRODUCTOS.java
@@ -0,0 +1,14 @@
+package com.jq.empresaalimentosyfrutas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Listar_PRODUCTOS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_productos);
+    }
+}
---
+++ app/src/main/java/com/jq/empresaalimentosyfrutas/Sub_Menu_PRODUCT.java
@@ -0,0 +1,14 @@
+package com.jq.empresaalimentosyfrutas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sub_Menu_PRODUCT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_menu_product);
+    }
+}
